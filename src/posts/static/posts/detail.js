d915f13af47422a19e6f05281dd8c996a4d0f965
Bhuwan Shrestha <bshrestha2146@conestogac.on.ca>
2024-04-06 03:15:11 -0400

    Implemented detail view for fetching post data via Ajax with dynamic UI adjustments

--- src/posts/static/posts/detail.js
+++ src/posts/static/posts/detail.js
@@ -1,8 +1,23 @@
 console.log('Hello World detail')
 
 const backBtn = document.getElementById('back-btn')
+const url = window.location.href + "data/"
+const spinnerBox = document.getElementById('spinner-box')
 
 backBtn.addEventListener('click',
     e => {
         history.back()
     })
+
+
+$.ajax({
+    type: 'GET',
+    url: url,
+    success: function(response) {
+        console.log(response)
+        spinnerBox.classList.add('not-visible')
+    },
+    error: function(error) {
+        console.log(error)
+    }
+})
