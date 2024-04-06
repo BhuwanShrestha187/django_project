0968e9aa711dfa3de3a17db886c4c4b56b0bea48
Bhuwan Shrestha <bshrestha2146@conestogac.on.ca>
2024-04-05 23:39:11 -0400

    Display dynamic 'Like'/'Unlike' status with counter on posts

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -10,18 +10,18 @@ let visible = 3
 
 const getData = () => {
 
-    $.ajax({
-        type: 'GET',
-        url: `/data/${visible}/`,
-        success: function(response) {
-            console.log(response)
-            const data = response.data
-            setTimeout(() => {
-                spinnerBox.classList.add('not-visible')
-                console.log(data)
+        $.ajax({
+                    type: 'GET',
+                    url: `/data/${visible}/`,
+                    success: function(response) {
+                            console.log(response)
+                            const data = response.data
+                            setTimeout(() => {
+                                        spinnerBox.classList.add('not-visible')
+                                        console.log(data)
 
-                data.forEach(el => {
-                    postsBox.innerHTML += `
+                                        data.forEach(el => {
+                                                    postsBox.innerHTML += `
                     <div class="card mb-2">
                         <div class="card-body">
                             <h5 class="card-title">${el.title}</h5>
@@ -30,12 +30,12 @@ const getData = () => {
 
                         <div class="card-footer">
                             <div class="row">
-                                <div class="col-1">
+                                <div class="col-2">
                                     <a href="#" class="btn btn-primary">Details</a>
                                 </div>
 
-                                <div class="col-1">
-                                    <a href="#" class="btn btn-primary">Like</a>
+                                <div class="col-2">
+                                    <a href="#" class="btn btn-primary">${el.liked ? `Unlike (${el.count})`: `Like (${el.count})`}</a>
                                 </div>
                             </div>
                             
