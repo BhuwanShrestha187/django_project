1f361f8ed0e96028d2b76be97a404fcd94b2341c
Bhuwan Shrestha <bshrestha2146@conestogac.on.ca>
2024-04-05 21:23:10 -0400

    Implemented spinner for loading state in Ajax calls

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -2,6 +2,7 @@ console.log('Hello World')
 
 const helloWorldBox = document.getElementById('hello-world')
 const postsBox = document.getElementById('posts-box')
+const spinnerBox = document.getElementById('spinner-box')
 
 $.ajax({
     type: 'GET',
@@ -21,13 +22,16 @@ $.ajax({
     success: function(response) {
         console.log(response)
         const data = response.data
-        console.log(data)
+        setTimeout(() => {
+            spinnerBox.classList.add('not-visible')
+            console.log(data)
 
-        data.forEach(el => {
-            postsBox.innerHTML += `
-                ${el.title} - <b>${el.body}</b><br>
-            `
-        });
+            data.forEach(el => {
+                postsBox.innerHTML += `
+                    ${el.title} - <b>${el.body}</b><br>
+                `
+            });
+        }, 100)
     },
     error: function(error) {
         console.log(error)
